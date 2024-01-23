99d4e8a0e69c61f48ea3b787fb1aa80e4b8a1dca
Naveen Ramesh <naveenramesh987@gmail.com>
2024-01-22 17:20:45 -0700

    Search for users

--- core/views.py
+++ core/views.py
@@ -12,6 +12,19 @@ import random
 def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
+    user_following_list = []
+    feed = []
+    user_following = FollowersCount.objects.filter(follower=request.user.username)
+
+    for users in user_following:
+        user_following_list.append(users.user)
+
+    for usernames in user_following_list:
+        feed_lists = Post.objects.filter(user=usernames)
+        feed.append(feed_lists)
+
+    feed_list = list(chain(*feed))
+
     posts = Post.objects.all()
     return render(request, "index.html", {"user_profile": user_profile, "posts": posts})
 
@@ -27,6 +40,32 @@ def upload(request):
         return redirect("/")
     else:
         return redirect("/")
+
+
+@login_required(login_url="signin")
+def search(request):
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        username_object = User.objects.filter(username__icontains=username)
+        username_profile = []
+        username_profile_list = []
+
+        for users in username_object:
+            username_profile.append(users.id)
+
+        for ids in username_profile:
+            profile_lists = Profile.objects.filter(id_user=ids)
+            username_profile_list.append(profile_lists)
+
+        username_profile_list = list(chain(*username_profile_list))
+    return render(
+        request,
+        "search.html",
+        {"user_profile": user_profile, "username_profile_list": username_profile_list},
+    )
 
 
 @login_required(login_url="signin")
